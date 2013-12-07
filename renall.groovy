ea38431923269ec59bb0ee42d126fc4307e772c6
rednoah <reinhard.pointner@gmail.com>
2013-12-07 15:35:49 +0800

    * support file-by-file rename mode out-of-the-box

--- renall.groovy
+++ renall.groovy
@@ -3,14 +3,17 @@
 /*
  * Rename all tv shows, anime or movies folder by folder using given or default options.
  */
-def target = tryQuietly{ target } ?: 'file' // target files by default
+def target = tryQuietly{ target } ?: 'file'
+def byfile = tryQuietly{ byfile.toBoolean() }
+
 
 args.eachMediaFolder {
-	if (it.isDisk())
+	if (it.isDisk()) {
 		return rename(file:it) // rename disk folders instead of files regardless of mode
+	}
 	
 	switch(target) {
-		case 'file'   :   return rename(folder:it) // rename files within each folder
+		case 'file'   :   return byfile ? it.listFiles().findAll{ it.isVideo() || it.isSubtitle() }.findResults{ rename(file:it) } : rename(folder:it) // rename files within each folder
 		case 'folder' :   return rename(file:it)   // rename folders as if they were files
 	}
 }
