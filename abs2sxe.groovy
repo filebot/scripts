0ad9c20dfa68c89ea2d945b13407b42e0d6e1ad3
rednoah <reinhard.pointner@gmail.com>
2013-12-19 15:37:38 +0800

    * non-strict is probably required

--- abs2sxe.groovy
+++ abs2sxe.groovy
@@ -1,4 +1,4 @@
-// filebot -script fn:abs2sxe <files>
+// filebot -script fn:abs2sxe <files>  [-non-strict]
 
 // normalize absolute number format and add title and release date info that we can match by in the second step
 def stage1 = rename(file:args.getFiles(), action:'rename', format:'{n} - {[e.pad(5)]} - {t} - {[d]}', order:'absolute', filter:'episode.season == null', db:'TheTVDB')
