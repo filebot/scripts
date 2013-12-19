d4d495b35bbaec6d01364cad96c9441238e74f48
rednoah <reinhard.pointner@gmail.com>
2013-12-19 15:31:54 +0800

    * added convencience script for converting absolute numbered episodes to SxE numbered episodes

---
+++ abs2sxe.groovy
@@ -0,0 +1,7 @@
+// filebot -script fn:abs2sxe <files>
+
+// normalize absolute number format and add title and release date info that we can match by in the second step
+def stage1 = rename(file:args.getFiles(), action:'rename', format:'{n} - {[e.pad(5)]} - {t} - {[d]}', order:'absolute', filter:'episode.season == null', db:'TheTVDB')
+
+// process again, match by title/date and output SxE according to --format
+rename(file:stage1, order:'airdate', db:'TheTVDB')
