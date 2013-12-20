59b5fcbf3118bec63b6fb72e4a4d0a832d174193
rednoah <reinhard.pointner@gmail.com>
2013-12-20 13:38:06 +0800

    * better performance using internal binding

--- amc.groovy
+++ amc.groovy
@@ -136,7 +136,7 @@ input = input.findAll{ f -> (f.isVideo() && !tryQuietly{ f.hasExtension('iso') &
 input = input.findAll{ f -> !(relativeInputPath(f) =~ /\b(?i:sample|trailer|extras|music.video|scrapbook|behind.the.scenes|extended.scenes|deleted.scenes|s\d{2}c\d{2}|mini.series)\b/) }
 
 // ignore files that don't conform with the file-size and video-length limits
-input = input.findAll{ f -> !(f.isFile() && ((minFileSize > 0 && f.length() < minFileSize) || (minLengthMS > 0 && tryQuietly{ getMediaInfo(file:f, format:'{media.Duration}').toLong() < minLengthMS }))) }
+input = input.findAll{ f -> !(f.isFile() && ((minFileSize > 0 && f.length() < minFileSize) || (minLengthMS > 0 && tryQuietly{ getMediaInfo(file:f, format:'{duration}').toLong() < minLengthMS }))) }
 
 // check and update exclude list (e.g. to make sure files are only processed once)
 if (excludeList) {
