63482e4ba825b1e97b922b39746df8dcd72536fb
rednoah <reinhard.pointner@gmail.com>
2013-12-27 13:05:21 +0800

    * script for renaming ebooks by title metadata

---
+++ sort.ebooks.groovy
@@ -0,0 +1,19 @@
+@Grab(group='org.apache.pdfbox', module='pdfbox', version='1.8.3')
+
+import org.apache.pdfbox.pdmodel.*
+
+args.getFiles{ f -> f.hasExtension('pdf') }.each { f->
+	def document = PDDocument.load(f)
+	def info = document.getDocumentInformation()
+	
+	// @see http://pdfbox.apache.org/cookbook/workingwithmetadata.html	
+	_log.finest "[$f.name] [$info.title] $info.dictionary"
+	
+	if (info.title) {
+		def dest = new File(f.parentFile, info.title.validateFileName() + '.pdf')
+		if (!f.equals(dest)) {
+			f.renameTo(dest)
+			println "[RENAME] [$f.name] to [$dest.name]"
+		}
+	}
+}
