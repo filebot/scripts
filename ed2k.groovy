8f580922a49411132e3315afe45edecf7bb17f0b
rednoah <reinhard.pointner@gmail.com>
2014-01-25 15:32:27 +0800

    * script for creating creating anidb style ed2k hashes

---
+++ ed2k.groovy
@@ -0,0 +1,17 @@
+// filebot -script fn:ed2k /path/to/anime
+
+/**
+ * Calculate ed2k for all video files and output as ed2k links for AniDB
+ *
+ * e.g.
+ * ed2k://|file|<filename, without spaces. unimportant>|<file size in bytes>|<ed2k hash>|/
+ */
+
+import net.sourceforge.filebot.hash.*
+
+args.getFiles{ it.isVideo() }.each{ f ->
+	def n = f.nameWithoutExtension.space('_') + '.' + f.extension
+	def l = f.length()
+	def h = VerificationUtilities.computeHash(f, HashType.ED2K);
+	println """ed2k://|file|${n}|${l}|${h}|/"""
+}
