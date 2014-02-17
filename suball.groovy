b7ece5842c02b3e6b97578b112a06b06bc51b92c
rednoah <reinhard.pointner@gmail.com>
2014-02-17 19:15:56 +0800

    * ignore any and all video files that already contain subtitle tracks
    @see http://www.filebot.net/forums/viewtopic.php?f=6&t=1025

--- suball.groovy
+++ suball.groovy
@@ -3,6 +3,11 @@
 /*
  * Get subtitles for all your media files  
  */
-args.eachMediaFolder { 
-	getMissingSubtitles(folder:it)
+args.eachMediaFolder {
+	def videos = it.listFiles{ it.isVideo() }
+
+	// ignore videos that already have embedded subtitles
+	videos = videos.findAll{ getMediaInfo(file:it, format:'''{media.TextCodecList}''').isEmpty() }
+
+	getMissingSubtitles(file:videos)
 }
