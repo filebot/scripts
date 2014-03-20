61afe88b23ddbcdffbbdddf4b23bac78d16067e2
rednoah <reinhard.pointner@gmail.com>
2014-03-20 15:46:42 +0800

    Version 2.0

--- abs2sxe.groovy
+++ abs2sxe.groovy
@@ -1,7 +1,20 @@
-// filebot -script fn:abs2sxe <files>  [-non-strict]
+// filebot -script fn:abs2sxe /path/to/files --log off
+
+def stage0 = args.getFiles{ it.isVideo() }.sort()
 
 // normalize absolute number format and add title and release date info that we can match by in the second step
-def stage1 = rename(file:args.getFiles(), action:'rename', format:'{n} - {[e.pad(5)]} - {t} - {[d]}', order:'absolute', filter:'episode.season == null', db:'TheTVDB')
+def stage1 = rename(file:stage0.sort(), action:'rename', format:'{n} - {[e.pad(5)]} - {t} - {[d]}', db:'TheTVDB', order:'absolute', filter:'episode.season == null && episode.special == null', strict:false)
 
 // process again, match by title/date and output SxE according to --format
-rename(file:stage1, order:'airdate', db:'TheTVDB')
+def stage2 = rename(file:stage1.sort(), db:'TheTVDB', order:'airdate', filter:'episode.special == null', strict:false)
+
+
+// print output statistics
+_log.info "\n-------------------- abs2sxe --------------------\n"
+
+def renameMap = getRenameLog()
+stage0.each{ f0 ->
+	def f1 = renameMap[f0]
+	def f2 = renameMap[f1]
+	println([f0, f1, f2].name.join('\t->\t'))
+}
