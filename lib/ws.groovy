a119c15f3df8fe757e55c3aa51bc9c68248f9778
rednoah <reinhard.pointner@gmail.com>
2014-03-22 00:35:09 +0800

    * PushBullet support

--- lib/ws.groovy
+++ lib/ws.groovy
@@ -49,3 +49,49 @@ class PushoverClient {
 		endpoint.post(parameters)
 	}
 }
+
+
+/****************************************************************************
+ * PushBullet
+ * 				https://www.pushbullet.com
+ ****************************************************************************/
+def PushBullet(apikey, devices = null) {
+	new PushBulletClient(apikey:apikey, devices:devices)
+}
+
+class PushBulletClient {
+	def apikey
+	def devices
+	
+	def url_devices = new URL('https://api.pushbullet.com/api/devices')
+	def url_pushes = new URL('https://api.pushbullet.com/api/pushes')
+	
+	def sendHtml = { title, message ->
+		def auth = "${apikey}".getBytes().encodeBase64().toString()
+		def header = [requestProperties: [Authorization: "Basic ${auth}" as String]]
+		
+		def response = new groovy.json.JsonSlurper().parse(url_devices, header)
+		def targets = response.devices.findAll{ devices == null || it.extras.nickname =~ devices }.findResults{ it.iden }
+		
+		targets.each{ device_iden ->
+			def contentType = 'multipart/form-data; boundary=----------------------------a1134e1059ac'
+			def multiPartFormData = """------------------------------a1134e1059ac
+Content-Disposition: form-data; name="device_iden"
+
+${device_iden}
+------------------------------a1134e1059ac
+Content-Disposition: form-data; name="type"
+
+file
+------------------------------a1134e1059ac
+Content-Disposition: form-data; name="file"; filename="${title}"
+Content-Type: text/html; charset=utf-8
+
+${message}
+------------------------------a1134e1059ac--
+""" as String
+
+			url_pushes.post(multiPartFormData, contentType, 'UTF-8', header.requestProperties)
+		}
+	}
+}
