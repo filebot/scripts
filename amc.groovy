ea91d8f87829f59cfb9e1ecfe6c4fb72ae9e4614
rednoah <reinhard.pointner@gmail.com>
2014-03-24 14:42:30 +0800

    * fix html markup issues and make sure html reports are html5 valid

--- amc.groovy
+++ amc.groovy
@@ -354,9 +354,10 @@ if (pushover) {
 def getReportSubject = { tryQuietly { ut_title } ?: input.collect{ relativeInputPath(it) as File }*.getRoot()*.getNameWithoutExtension().unique().sort{ it.toLowerCase() }.join(', ') }
 def getReportMessage = { 
 	def renameLog = getRenameLog()
-	XML {
+	return '<!DOCTYPE html>\n' + XML {
 		html {
 			head {
+				meta(charset:'UTF-8')
 				style('''
 					p{font-family:Arial,Helvetica,sans-serif}
 					p b{color:#07a}
@@ -365,20 +366,21 @@ def getReportMessage = {
 					table a:link{color:#666;font-weight:bold;text-decoration:none}
 					table a:visited{color:#999;font-weight:bold;text-decoration:none}
 					table a:active,table a:hover{color:#bd5a35;text-decoration:underline}
-					table{font-family:Arial,Helvetica,sans-serif;color:#666;font-size:12px;text-shadow:1px 1px 0 #fff;background:#eaebec;margin:15px;border:#ccc 1px solid;-moz-border-radius:3px;-webkit-border-radius:3px;border-radius:3px;-moz-box-shadow:0 1px 2px #d1d1d1;-webkit-box-shadow:0 1px 2px #d1d1d1;box-shadow:0 1px 2px #d1d1d1}
-					table th{padding:15px;border-top:1px solid #fafafa;border-bottom:1px solid #e0e0e0;background:#ededed;background:-webkit-gradient(linear,left top,left bottom,from(#ededed),to(#ebebeb));background:-moz-linear-gradient(top,#ededed,#ebebeb)}
+					table{font-family:Arial,Helvetica,sans-serif;color:#666;background:#eaebec;margin:15px;border:#ccc 1px solid;border-radius:3px;box-shadow:0 1px 2px #d1d1d1}
+					table th{padding:15px;border-top:1px solid #fafafa;border-bottom:1px solid #e0e0e0;background:#ededed}
 					table th{text-align:center;padding-left:20px}
-					table tr:first-child th:first-child{-moz-border-radius-topleft:3px;-webkit-border-top-left-radius:3px;border-top-left-radius:3px}
-					table tr:first-child th:last-child{-moz-border-radius-topright:3px;-webkit-border-top-right-radius:3px;border-top-right-radius:3px}
+					table tr:first-child th:first-child{border-top-left-radius:3px}
+					table tr:first-child th:last-child{border-top-right-radius:3px}
 					table tr{text-align:left;padding-left:20px}
 					table td:first-child{text-align:left;padding-left:20px;border-left:0}
-					table td{padding:15px;border-top:1px solid #fff;border-bottom:1px solid #e0e0e0;border-left:1px solid #e0e0e0;background:#fafafa;background:-webkit-gradient(linear,left top,left bottom,from(#fbfbfb),to(#fafafa));background:-moz-linear-gradient(top,#fbfbfb,#fafafa);white-space:nowrap}
-					table tr.even td{background:#f6f6f6;background:-webkit-gradient(linear,left top,left bottom,from(#f8f8f8),to(#f6f6f6));background:-moz-linear-gradient(top,#f8f8f8,#f6f6f6)}
+					table td{padding:15px;border-top:1px solid #fff;border-bottom:1px solid #e0e0e0;border-left:1px solid #e0e0e0;background:#fafafa;white-space:nowrap}
+					table tr.even td{background:#f6f6f6}
 					table tr:last-child td{border-bottom:0}
-					table tr:last-child td:first-child{-moz-border-radius-bottomleft:3px;-webkit-border-bottom-left-radius:3px;border-bottom-left-radius:3px}
-					table tr:last-child td:last-child{-moz-border-radius-bottomright:3px;-webkit-border-bottom-right-radius:3px;border-bottom-right-radius:3px}
-					table tr:hover td{background:#f2f2f2;background:-webkit-gradient(linear,left top,left bottom,from(#f2f2f2),to(#f0f0f0));background:-moz-linear-gradient(top,#f2f2f2,#f0f0f0)}
-				''') // @see http://johnsardine.com/freebies/dl-html-css/simple-little-tab/
+					table tr:last-child td:first-child{border-bottom-left-radius:3px}
+					table tr:last-child td:last-child{border-bottom-right-radius:3px}
+					table tr:hover td{background:#f2f2f2}
+				''')
+				title("[FileBot] ${getReportSubject()}")
 			}
 			body {
 				p {
@@ -387,7 +389,7 @@ def getReportMessage = {
 					mkp.yield(" (${renameLog.size()} files).")
 				}
 				hr(); table {
-					th('Original Name'); th('New Name')
+					tr { th('Original Name'); th('New Name') }
 					renameLog.each{ from, to ->
 						tr { [from.name, to.name].each{ cell -> td(cell) } }
 					}
