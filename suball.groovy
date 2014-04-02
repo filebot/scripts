966cec610d4fdf41a98126841f28beb6c19aba5d
rednoah <reinhard.pointner@gmail.com>
2014-04-02 18:21:16 +0800

    Support --def maxAgeDays argument

--- suball.groovy
+++ suball.groovy
@@ -1,13 +1,33 @@
 // filebot -script fn:suball <options> <folder>
 
+
+def accept(File f) {
+	// ignore files that are too old
+	if (f.lastModified() < System.currentTimeMillis() - tryQuietly{ maxAgeDays.toLong() * 24 * 60 * 60 * 1000 })
+		return false
+	
+	// ignore files that already have subtitles
+	if (getMediaInfo(file:f, format:'''{media.TextCodecList}''').isEmpty())
+		return false
+	
+	return true
+}
+
+
 /*
  * Get subtitles for all your media files  
  */
 args.eachMediaFolder {
 	def videos = it.listFiles{ it.isVideo() }
-
+	
 	// ignore videos that already have embedded subtitles
-	videos = videos.findAll{ getMediaInfo(file:it, format:'''{media.TextCodecList}''').isEmpty() }
-
+	videos = videos.findAll{ 
+		if (!accept(it)) {
+			_log.finest "Exclude: " + it
+			return false
+		}
+		return true
+	}
+	
 	getMissingSubtitles(file:videos)
 }
