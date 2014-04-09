67ba26921fba7c742d7555b37053e91662848e29
rednoah <reinhard.pointner@gmail.com>
2014-04-09 16:14:17 +0800

    added support for --def minFileSize and --def minLengthMS

--- suball.groovy
+++ suball.groovy
@@ -2,10 +2,21 @@
 
 
 def lastModifiedLimit = tryQuietly{ System.currentTimeMillis() - (maxAgeDays.toLong() * 24 * 60 * 60 * 1000) }
+def minFileSize = tryQuietly{ minFileSize.toLong() }; if (minFileSize == null) { minFileSize = 50 * 1000L * 1000L }
+def minLengthMS = tryQuietly{ minLengthMS.toLong() }; if (minLengthMS == null) { minLengthMS = 10 * 60 * 1000L }
+
 
 def accept = { f ->
 	// ignore files that are too old
 	if (lastModifiedLimit != null && f.lastModified() < lastModifiedLimit)
+		return false
+	
+	// ignore files that are too small	
+	if (minFileSize > 0 && f.length() < minFileSize)
+		return false
+	
+	// ignore files that are too short
+	if ((minLengthMS > 0 && tryQuietly{ getMediaInfo(file:f, format:'{duration}').toLong() < minLengthMS }))
 		return false
 	
 	// ignore files that already have subtitles
