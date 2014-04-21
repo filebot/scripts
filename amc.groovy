98079d6b4994ccd3ee7644028f7473cd2bc54786
rednoah <reinhard.pointner@gmail.com>
2014-04-21 14:48:33 +0800

    * remove clutter from reports

--- amc.groovy
+++ amc.groovy
@@ -195,21 +195,6 @@ def groups = input.groupBy{ f ->
 		def sn = norm(tvs)
 		def mn = norm(mov.name)
 		
-		/**
-		println '--- EPISODE FILTER (POS) ---'
-		println parseEpisodeNumber(fn, true) || parseDate(fn)
-		println ([dn, fn].find{ it =~ sn && matchMovie(it) == null } && (parseEpisodeNumber(stripReleaseInfo(fn.after(sn), false), false) || fn.after(sn) =~ /\D\d{1,2}\D{1,3}\d{1,2}\D/) && matchMovie(fn, true) == null)
-		println (fn.after(sn) ==~ /.{0,3} - .+/ && matchMovie(fn, true) == null)
-		println f.dir.listFiles{ it.isVideo() && (dn =~ sn || norm(it.name) =~ sn) && it.name =~ /\d{1,3}/}.findResults{ it.name.matchAll(/\d{1,3}/) as Set }.unique().size() >= 10
-		println '--- EPISODE FILTER (NEG) ---'
-		println (mov.year >= 1950 && f.listPath().reverse().take(3).find{ it.name =~ mov.year })
-		println (mn =~ sn && [dn, fn].find{ it =~ /(19|20)\d{2}/ })
-		println '--- MOVIE FILTER (POS) ---'
-		println (mn.getSimilarity(fn) >= 0.8 || [dn, fn].find{ it.findAll( ~/\d{4}/ ).findAll{ y -> [mov.year-1, mov.year, mov.year+1].contains(y.toInteger()) }.size() > 0 } != null)
-		println ([dn, fn].find{ it =~ mn && !(it.after(mn) =~ /\b\d{1,3}\b/) && (it.getSimilarity(mn) > 0.2 + it.getSimilarity(sn)) } != null)
-		println (detectMovie(f, true) && [dn, fn].find{ it =~ /(19|20)\d{2}/ } != null)
-		**/
-		
 		// S00E00 | 2012.07.21 | One Piece 217 | Firefly - Serenity | [Taken 1, Taken 2, Taken 3, Taken 4, ..., Taken 10]
 		if ((parseEpisodeNumber(fn, true) || parseDate(fn) || ([dn, fn].find{ it =~ sn && matchMovie(it) == null } && (parseEpisodeNumber(stripReleaseInfo(fn.after(sn), false), false) || fn.after(sn) =~ /\D\d{1,2}\D{1,3}\d{1,2}\D/) && matchMovie(fn, true) == null) || (fn.after(sn) ==~ /.{0,3} - .+/ && matchMovie(fn, true) == null) || f.dir.listFiles{ it.isVideo() && (dn =~ sn || norm(it.name) =~ sn) && it.name =~ /\d{1,3}/}.findResults{ it.name.matchAll(/\d{1,3}/) as Set }.unique().size() >= 10 || mov.year < 1900) && !( (mov.year >= 1950 && f.listPath().reverse().take(3).find{ it.name =~ mov.year }) || (mn =~ sn && [dn, fn].find{ it =~ /(19|20)\d{2}/ }) ) ) {
 			log.fine("Exclude Movie: $mov")
@@ -378,6 +363,10 @@ if (excludeList) {
 
 
 if (getRenameLog().size() == 0) die("Finished without processing any files")
+
+
+// exclude subtitles from reporting to reduce clutter
+if (input.find{ !it.isSubtitle() }) input = input.findAll{ !it.isSubtitle() }
 
 
 // messages used for xbmc / plex / pushover notifications
