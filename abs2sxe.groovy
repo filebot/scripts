76187cb29917faaf50c21520ad148b88089b6a5f
rednoah <reinhard.pointner@gmail.com>
2014-07-27 00:28:03 +0800

    * error message if the first temporary rename step fails

--- abs2sxe.groovy
+++ abs2sxe.groovy
@@ -5,6 +5,8 @@ def stage0 = args.getFiles{ it.isVideo() }.sort()
 // normalize absolute number format and add title and release date info that we can match by in the second step
 def stage1 = rename(file:stage0.sort(), action:'rename', format:'{n} - {[e.pad(5)]} - {t} - {[d]}', db:'TheTVDB', order:'absolute', filter:'episode.season == null && episode.special == null', strict:false)
 
+if (stage1 == null) die('Failure at Stage 0')
+
 // process again, match by title/date and output SxE according to --format
 def stage2 = rename(file:stage1.sort(), db:'TheTVDB', order:'airdate', filter:'episode.special == null', strict:false)
 
