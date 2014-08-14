9d934b7a6d6d4940b26c51b1fcd2607ae82f9410
rednoah <reinhard.pointner@gmail.com>
2014-08-14 16:14:44 +0800

    * add spaces between categories

--- sysenv.groovy
+++ sysenv.groovy
@@ -4,13 +4,16 @@ println '# Environment Variables #'
 _environment.each{
 	println "$it.key: $it.value"
 }
+println '\n'
 
 println '# Java System Properties #'
 _system.each{
 	println "$it.key: $it.value"
 }
+println '\n'
 
 println '# Arguments #'
 _args.array.eachWithIndex{ a, i ->
 	println "args[$i] = $a"
 }
+println '\n'
