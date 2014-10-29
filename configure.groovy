db7029ff63bb8cb12232a6f3e9eed65da6b2740b
rednoah <reinhard.pointner@gmail.com>
2014-10-29 12:16:52 +0800

    * added general setup script for filebot settings (though currently only used for setting OpenSubtitles login)

---
+++ configure.groovy
@@ -0,0 +1,25 @@
+// filebot -script fn:configure
+
+console.print('Enter OpenSubtitles username: ')
+def osdbUser = console.readLine()
+console.print('Enter OpenSubtitles password: ')
+def osdbPwd = console.readLine()
+
+
+setLogin('osdb.user', osdbUser, osdbPwd)
+
+
+/* --------------------------------------------------------------------- */
+
+if (osdbUser) {
+	console.print('Testing OpenSubtitles... ')
+	WebServices.OpenSubtitles.setUser(osdbUser, osdbPwd)
+	WebServices.OpenSubtitles.login()
+	console.println('OK')
+}
+
+/* --------------------------------------------------------------------- */
+
+def setLogin(key, user, pwd) {
+	Settings.forPackage(WebServices.class).put(key, [user, pwd].join(':'))
+}
