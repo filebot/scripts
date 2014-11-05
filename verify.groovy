6f55422ecbefc92c47230c7a22e53951fc61907c
rednoah <reinhard.pointner@gmail.com>
2014-11-05 14:58:09 +0800

    * added file verification script that stores previously computed CRC32 checksums as xattr metadata, and uses previously stored xattr metadata to check file integrity in future runs

---
+++ verify.groovy
@@ -0,0 +1,38 @@
+// filebot -script fn:verify /path/to/files
+
+import java.util.concurrent.*
+import java.nio.channels.*
+import java.util.zip.*
+import net.filebot.hash.*
+
+
+def hashType = HashType.SFV
+def xattrkey = 'CRC32'
+
+def files = args.getFiles{ it.isVideo() }
+
+def threadPoolSize = Runtime.getRuntime().availableProcessors()
+def executor = Executors.newFixedThreadPool(threadPoolSize)
+
+executor.invokeAll(files.collect{ f ->
+	return {
+		def attr_hash = f.xattr[xattrkey]
+		def calc_hash = VerificationUtilities.computeHash(f, hashType)
+		
+		log.info "$attr_hash $calc_hash $f"
+
+		if (attr_hash == null) {
+			log.warning "Set xattr $xattrkey for [$f]"
+			f.xattr[xattrkey] = calc_hash
+
+			// verify that xattr has been set correctly
+			if (f.xattr[xattrkey] != calc_hash) {
+				log.severe "Failed to set xattr $xattrkey for [$f]"
+				System.exit(-1)
+			}
+		} else if (attr_hash == calc_hash) {
+			log.severe "$xattrkey mismatch (expected $attr_hash, actual: $calc_hash) for [$f]"
+			System.exit(-1)
+		}
+	}
+})*.get()
