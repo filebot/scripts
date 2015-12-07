bd25984615de454fc8d0cd96277de3e95088e9e3
rednoah <rednoah@users.sourceforge.net>
2015-12-07 13:56:44 +0800

    remove old code

--- replace.groovy
+++ replace.groovy
@@ -1,7 +1,6 @@
 // filebot -script fn:replace --action test --filter "[.]srt$" --def "e=[.](eng|english)" "r=.en"
 
 
-def action = StandardRenameAction.forName(_args.action)
 def accept = { f -> _args.filter ? f.path =~ _args.filter : true }
 def pattern = Pattern.compile(e, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE)
 
