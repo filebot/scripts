54e19c0a82c31d124ed3e2f9796f69500a35f951
rednoah <rednoah@users.sourceforge.net>
2016-02-24 14:31:05 +0800

    Print ignore reasons

--- suball.groovy
+++ suball.groovy
@@ -15,31 +15,44 @@ def minLengthMS = minLengthMS as long
 def minAgeTimeStamp = any{ now.time - ((minAgeDays as double) * 24 * 60 * 60 * 1000L) as long }{ null }
 def maxAgeTimeStamp = any{ now.time - ((maxAgeDays as double) * 24 * 60 * 60 * 1000L) as long }{ null }
 
-def accept = { f ->
-	// ignore files that match the give ignore pattern
-	if (f.path =~ ignore)
-		return false
+def ignore = { f, m ->
+	log.finest "Ignore [$f.name]: $m"
+	return false
+}
 
-	// ignore files that are too young
-	if (minAgeTimeStamp != null && f.creationDate > minAgeTimeStamp)
-		return false
+def accept = { f ->
+	def creationDate = f.creationDate
 
 	// ignore files that are too old
-	if (maxAgeTimeStamp != null && f.creationDate < maxAgeTimeStamp)
-		return false
-	
+	if (maxAgeTimeStamp != null && creationDate < maxAgeTimeStamp) {
+		return ignore(f, 'File creation date is too far in the past')
+	}
+
+	// ignore files that are too young
+	if (minAgeTimeStamp != null && creationDate > minAgeTimeStamp) {
+		return ignore(f, 'File creation date is too recent')
+	}
+
+	// ignore files that match the give ignore pattern
+	if (ignore != null && f.path =~ ignore) {
+		return ignore(f, 'File path matches the ignore pattern')
+	}
+
 	// ignore files that are too small	
-	if (minFileSize > 0 && f.length() < minFileSize)
-		return false
-	
+	if (minFileSize > 0 && f.length() < minFileSize) {
+		return ignore(f, 'File size is too small')
+	}
+
 	// ignore files that are too short
-	if (minLengthMS > 0 && ((getMediaInfo(file:f, format:'{duration}') ?: minLengthMS) as double) < minLengthMS)
-		return false
-	
+	if (minLengthMS > 0 && ((getMediaInfo(file:f, format:'{duration}') ?: minLengthMS) as double) < minLengthMS) {
+		return ignore(f, 'Video duration is too short')
+	}
+
 	// ignore files that already have subtitles
-	if (ignoreTextLanguage != null && getMediaInfo(file:f, format:'{media.TextLanguageList}').findMatch(ignoreTextLanguage) != null)
-		return false
-	
+	if (ignoreTextLanguage != null && getMediaInfo(file:f, format:'{media.TextLanguageList}').findMatch(ignoreTextLanguage) != null) {
+		return ignore(f, 'Video file already contains embedded subtitles')
+	}
+
 	return true
 }
 
@@ -48,17 +61,12 @@ def accept = { f ->
  * Get subtitles for all your media files  
  */
 args.eachMediaFolder { dir ->
-	def input = dir.listFiles{ f -> f.isVideo() }
-	def selected = input.findAll{ f -> accept(f) }
-		
-	if (selected.size() > 0) {
-		log.info "Fetch subtitles for [$dir]"
-		
-		// print excludes
-		(input - selected).each{ f -> log.finest "Ignore: $f" }
+	def files = dir.listFiles{ f -> f.isVideo() && accept(f) }
 
-		getMissingSubtitles(file: selected)
+	if (files.size() > 0) {
+		log.info "Fetch subtitles for [$dir]"
+		getMissingSubtitles(file: files)
 	} else {
-		log.finest "Exclude: $dir"
+		log.fine "Exclude: $dir"
 	}
 }
