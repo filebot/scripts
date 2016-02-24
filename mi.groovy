d04a7daa9fca3e6bdfaa0cf37e816b1734245c18
rednoah <rednoah@users.sourceforge.net>
2016-02-24 10:43:54 +0800

    Improved support for | pipe usage

--- mi.groovy
+++ mi.groovy
@@ -3,25 +3,36 @@
 /*
  * Print media info of all video files to CSV file  
  */
-def model = '''Name;Container;Resolution;Video Codec;Video Format;Audio Codec;Audio Format;Audio Language(s);Subtitle Language(s);Duration;File Size;Folder Size;Folder Count;Path'''
+def model    = '''Name;Container;Resolution;Video Codec;Video Format;Audio Codec;Audio Format;Audio Language(s);Subtitle Language(s);Duration;File Size;Folder Size;Folder Count;Path'''
 def template = '''{fn};{cf};{resolution};{vc};{vf};{ac};{af};{media.AudioLanguageList};{media.TextLanguageList};{media.DurationString3};{file.length()};{folder.listFiles().sum{ it.length() }};{folder.listFiles().sum{ it.isFile() ? 1 : 0 }};{file.getCanonicalPath()}'''
 
-// sanity check
-if (args.size() != 2) die('Invalid arguments:' + args)
+def csvFile  = args[-1] as File
 
-// open destination file (writing files requires -trust-script)
-args[1].withWriter{ output ->
-	// print header
-	output.writeLine(model)
-	
-	// print info for each video file (sorted by filename)
-	args[0].getFiles{ it.isVideo() }.sort{ a, b -> a.name.compareToIgnoreCase(b.name) }.each{
-		def mi = getMediaInfo(file:it, format:template)
-		
+if (csvFile.name.endsWith('.csv')) {
+	// open destination file (writing files requires -trust-script)
+	csvFile.withWriter('UTF-8'){ output ->
 		// print to console
-		println mi
-		
-		// append to file
-		output.writeLine(mi)
-	}
+		println "Writing CSV file [$csvFile]"
+		println model
+
+		// print header
+		output.write(model)
+		output.write('\n')
+
+		// print info for each video file (sorted by filename)
+		args.getFiles{ it.isVideo() }.sort{ a, b -> a.name.compareToIgnoreCase(b.name) }.each{
+			def mi = getMediaInfo(file:it, format:template)
+
+			// print to console
+			println mi
+
+			// append to file
+			output.write(mi)
+			output.write('\n')
+		}
+	}	
+} else {
+	// pipe usage
+	getMediaInfo(args.getFiles{ it.isVideo() }, template).each{ println it }
+	System.exit(0)
 }
