ed8e5cc2b44fdefdfb2d6d4fefb7def16167bcd9
rednoah <rednoah@users.sourceforge.net>
2016-03-03 23:31:31 +0800

    Change history default format to ${from}\t${to}

--- history.groovy
+++ history.groovy
@@ -1,12 +1,12 @@
 // filebot -script fn:history --format "$from => $to"
 
 // use --format parameter to specify your own output format
-def format = _args.format ?: '[$from.name] => [$to.name]'
+def format = _args.format ?: '''${from}\t${to}'''
 def template = new groovy.text.GStringTemplateEngine().createTemplate(format)
 
 // use args to list history only for the given folders if desired
 def accept(from, to) {
-	args.empty ? true : args.find{ to.absolutePath.startsWith(it.absolutePath) } && to.exists()
+	return args.empty || (args.any{ to.absolutePath.startsWith(it.absolutePath) } && to.exists())
 }
 
 
