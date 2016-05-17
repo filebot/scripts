921d3f09cdbf44a4f7031e944a433cf19e30784a
rednoah <rednoah@users.sourceforge.net>
2016-05-18 00:26:18 +0800

    Disable artwork override (avoid abuse)

--- artwork.tmdb.groovy
+++ artwork.tmdb.groovy
@@ -1,7 +1,6 @@
 // filebot -script fn:artwork.tmdb /path/to/movies/
 
 def extras = any{ extras.toBoolean() }{ false }
-def override = _args.conflict == 'override'
 
 
 include('lib/htpc')
@@ -9,15 +8,15 @@ include('lib/htpc')
 
 args.eachMediaFolder{ dir ->
 	// fetch only missing artwork by default
-	if (!override && dir.hasFile{it.name == 'movie.nfo'} && dir.hasFile{it.name == 'poster.jpg'} && dir.hasFile{it.name == 'fanart.jpg'}) {
+	if (dir.hasFile{it.name == 'movie.nfo'} && dir.hasFile{it.name == 'poster.jpg'} && dir.hasFile{it.name == 'fanart.jpg'}) {
 		println "Skipping $dir"
 		return
 	}
-	
+
 	def videos = dir.listFiles{ it.isVideo() } as List
 	def query = _args.query
 	def options = []
-	
+
 	if (query) {
 		// manual search & sort by relevance
 		options = TheMovieDB.searchMovie(query, _args.locale).sortBySimilarity(query, { it.name })
@@ -25,24 +24,24 @@ args.eachMediaFolder{ dir ->
 		// run movie auto-detection for video files
 		options = MediaDetection.detectMovie(videos[0], TheMovieDB, _args.locale, true)
 	}
-	
+
 	if (options.isEmpty()) {
 		println "$dir ${videos.name} => movie not found"
 		return
 	}
-	
+
 	// auto-select movie
 	def movie = options[0]
-	
+
 	// maybe require user input
 	if (options.size() != 1 && !_args.nonStrict && !java.awt.GraphicsEnvironment.headless) {
 		movie = javax.swing.JOptionPane.showInputDialog(null, 'Please select Movie:', dir.name, 3, null, options.toArray(), movie)
 		if (movie == null) return null
 	}
-	
+
 	println "$dir => $movie"
 	try {
-		fetchMovieArtworkAndNfo(dir, movie, dir.getFiles{ it.isVideo() }.sort{ it.length() }.reverse().findResult{ it }, extras, override, _args.locale ?: Locale.ENGLISH)
+		fetchMovieArtworkAndNfo(dir, movie, dir.getFiles{ it.isVideo() }.sort{ it.length() }.reverse().findResult{ it }, extras, false, _args.locale ?: Locale.ENGLISH)
 	} catch(e) {
 		println "${e.class.simpleName}: ${e.message}"
 	}
--- artwork.tvdb.groovy
+++ artwork.tvdb.groovy
@@ -1,49 +1,47 @@
 // filebot -script fn:artwork.tvdb /path/to/tv/
 
-def override = _args.conflict == 'override'
-
 
 include('lib/htpc')
 
 
 args.eachMediaFolder{ dir ->
 	// fetch only missing artwork by default
-	if (!override && dir.hasFile{it.name == 'banner.jpg'}) {
+	if (dir.hasFile{it.name == 'banner.jpg'}) {
 		println "Skipping $dir"
 		return
 	}
-	
+
 	def videos = dir.listFiles{ it.isVideo() }
 	def query = _args.query ?: detectSeriesName(videos)
 	def sxe = videos.findResult{ parseEpisodeNumber(it) }
-	
+
 	if (query == null) {
 		query = dir.dir.hasFile{ it.name =~ /Season/ && it.isDirectory() } ? dir.dir.name : dir.name
 	}
-	
+
 	println "$dir => Search by $query"
 	def options = TheTVDB.search(query, _args.locale)
 	if (options.isEmpty()) {
 		println "TV Series not found: $query"
 		return
 	}
-	
+
 	// sort by relevance
 	options = options.sortBySimilarity(query, { it.name })
-	
+
 	// auto-select series
 	def series = options[0]
-	
+
 	// maybe require user input
 	if (options.size() != 1 && !_args.nonStrict && !java.awt.GraphicsEnvironment.headless) {
 		series = javax.swing.JOptionPane.showInputDialog(null, 'Please select TV Show:', dir.name, 3, null, options.toArray(), series)
 		if (series == null) return
 	}
-	
+
 	// auto-detect structure
 	def seriesDir = [dir.dir, dir].sortBySimilarity(series.name, { it.name })[0]
 	def season = sxe && sxe.season > 0 ? sxe.season : 1
-	
+
 	println "$dir => $series"
-	fetchSeriesArtworkAndNfo(seriesDir, dir, series, season, override, _args.locale ?: Locale.ENGLISH)
+	fetchSeriesArtworkAndNfo(seriesDir, dir, series, season, false, _args.locale ?: Locale.ENGLISH)
 }
