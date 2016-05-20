378b46abfc90f51a11f9e86524e56bde11ef255f
rednoah <rednoah@users.sourceforge.net>
2016-05-20 15:44:09 +0800

    Fix type issues

--- duplicates.groovy
+++ duplicates.groovy
@@ -2,7 +2,7 @@
 
 args.getFiles{ it.isVideo() }.groupBy{ it.metadata }.each{ m, fs ->
 	if (m && fs.size() > 1) {
-		log.info m
+		log.info "[*] $m"
 
 		fs.toSorted(new VideoQuality()).eachWithIndex{ f, i ->
 			if (i == 0) {
