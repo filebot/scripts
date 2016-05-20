68d05c65d18559b95a6c09bdf75e3364e406c185
rednoah <rednoah@users.sourceforge.net>
2016-05-20 14:04:53 +0800

    + added duplicate find & delete script

---
+++ duplicates.groovy
@@ -0,0 +1,19 @@
+// filebot -script fn:duplicates --action delete /path/to/media
+
+args.getFiles{ it.isVideo() }.groupBy{ it.metadata }.each{ m, fs ->
+	if (m && fs.size() > 1) {
+		log.info m
+
+		fs.toSorted(new VideoQuality()).eachWithIndex{ f, i ->
+			if (i == 0) {
+				log.fine "[+] 1. $f"
+			} else {
+				log.fine "[-] ${i+1}. $f"
+
+				if ('delete' == _args.action) {
+					f.delete()
+				}
+			}
+		}
+	}
+}
