66e0a1b274876415d02ec727a19b6b7d06d62de5
rednoah <rednoah@users.sourceforge.net>
2016-06-20 23:17:43 +0800

    NOTE: some smb filesystem implementations are buggy and known to incorrectly return filesize 0 for all files

--- cleaner.groovy
+++ cleaner.groovy
@@ -30,7 +30,8 @@ def isClutter = { f ->
 	if (f.path =~ pathExcludePattern && fsize < maxsize)
 		return true
 
-	if ((f.isVideo() || f.isAudio()) && fsize < minsize)
+	// NOTE: some smb filesystem implementations are buggy and known to incorrectly return filesize 0 for all files
+	if ((f.isVideo() || f.isAudio()) && fsize < minsize && fsize > 0)
 		return true
 
 	return false
