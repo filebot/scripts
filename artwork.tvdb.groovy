c42f5bcd75f3fc8a8cde6f4fdadd12756438e55f
rednoah <rednoah@users.sourceforge.net>
2016-06-23 23:15:24 +0800

    Fix compatibility issue
    @see https://www.filebot.net/forums/viewtopic.php?f=4&t=3891&p=21887#p21887

--- artwork.tvdb.groovy
+++ artwork.tvdb.groovy
@@ -43,5 +43,5 @@ args.eachMediaFolder{ dir ->
 	def season = sxe && sxe.season > 0 ? sxe.season : 1
 
 	println "$dir => $series"
-	fetchSeriesArtworkAndNfo(seriesDir, dir, series, season, false, _args.locale ?: Locale.ENGLISH)
+	fetchSeriesArtworkAndNfo(seriesDir, dir, series.id, season, false, _args.locale ?: Locale.ENGLISH)
 }
