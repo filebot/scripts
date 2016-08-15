cac4ced45a967a2f2b73d90477c97c9d07bcb36f
rednoah <rednoah@users.sourceforge.net>
2016-08-15 23:36:37 +0800

    Support finer-grained logging output

--- explain.groovy
+++ explain.groovy
@@ -2,17 +2,14 @@
 
 
 args.getFiles{ it.isVideo() }.each{ f ->
-	println ' File / Object / MediaInfo '.center(80, '-')
-
-	println 'File:   ' + f
-	println 'Object: ' + f.xattr['net.filebot.metadata']
-	println 'Media:  ' + any{ MediaInfo.snapshot(f) }{ null }
+	log.info 'File:   ' + f
+	log.info 'Object: ' + f.xattr['net.filebot.metadata']
+	log.info 'Media:  ' + any{ MediaInfo.snapshot(f) }{ null }
 
 	if (f.metadata) {
-		println ' Episode Metrics '.center(80, '-')
 		EpisodeMetrics.defaultSequence(false).each{ m ->
-			print m.name().padRight(20, ' ')
-			println m.getSimilarity(f, f.metadata).round(1)
+			log.finest m.name().padRight(20, ' ')
+			log.finest m.getSimilarity(f, f.metadata).round(1)
 		}
 	}
 }
