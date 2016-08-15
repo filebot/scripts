79d496889f35632e1b9582f60378b17b3ba5a9c5
rednoah <rednoah@users.sourceforge.net>
2016-08-15 23:18:39 +0800

    Include script bundle revision / build-date

--- sysinfo.groovy
+++ sysinfo.groovy
@@ -6,17 +6,17 @@ println Settings.getApplicationIdentifier()
 // JNA Native: 3.5.0
 try {
 	print 'JNA Native: '
-	println com.sun.jna.Native.nativeVersion
+	println com.sun.jna.Native.getNativeVersion()
 } catch(Throwable error) {
-	println error.cause ?: error.message
+	println error
 }
 
 // MediaInfo: MediaInfoLib - v0.7.48
 try {
 	print 'MediaInfo: '
-	println MediaInfo.version()
+	println MediaInfo.version().replaceAll(/[^\d_.]/, '')
 } catch(Throwable error) {
-	println error.cause ?: error.message
+	println error
 }
 
 // 7-Zip-JBinding: OK
@@ -24,7 +24,7 @@ try {
 	switch (System.getProperty('net.filebot.Archive.extractor')) {
 		case 'ApacheVFS':
 			print 'Apache Commons VFS: '
-			println (org.apache.commons.vfs2.VFS.manager.schemes - ['file', 'gz', 'bz2', 'par', 'res', 'sar', 'war', 'tmp', 'ear', 'ejb3', 'jar', 'ram'])
+			println org.apache.commons.vfs2.VFS.manager.schemes.findAll{ !(it =~ /file|gz|bz2|par|res|sar|war|tmp|ear|ejb3|jar|ram|ftp/) }
 			break
 		case 'SevenZipExecutable':
 			print 'p7zip: '
@@ -43,9 +43,8 @@ try {
 // chromaprint-tools
 try {
 	print 'Chromaprint: '
-	def fpcalc = AcoustID.getChromaprintCommand()
-	def version = [fpcalc, '-version'].execute().text.trim() ?: 'fpcalc -version failed'
-	println "$version ($fpcalc)"
+	def fpcalc = [AcoustID.getChromaprintCommand(), '-version']
+	println fpcalc.execute().text.replaceAll(/[^\d_.]/, '') ?: "$fpcalc failed"
 } catch(Throwable error) {
 	println error
 }
@@ -55,14 +54,15 @@ try {
 	print 'Extended Attributes: '
 	if (Settings.useExtendedFileAttributes()){
 		// create new temp file
-		def f = new File(Settings.getApplicationFolder(), '.xattr-test')
+		def f = new File(Settings.getApplicationFolder(), '.xattr')
 		f.createNewFile() && f.deleteOnExit()
 
 		// xattr write, read and verify
 		def xattr = new MetaAttributes(f)
-		def payload = new Date()
-		xattr.setObject(payload)
-		assert xattr.getObject() == payload
+		def value = new Date()
+		xattr.setObject(value)
+
+		assert xattr.getObject() == value
 		println 'OK'
 	} else {
 		println 'DISABLED'
@@ -82,28 +82,43 @@ try {
 	println error
 }
 
+// Script Bundle: 2016-08-03 (r389)
+try {
+	print "Script Bundle: "
+	def manifest = net.filebot.cli.ScriptSource.GITHUB_STABLE.getScriptProvider(null).getManifest()
+	def r = manifest['Build-Revision']
+	def d = manifest['Build-Date']
+	println "$d (r$r)"
+} catch(Throwable error) {
+	println error
+}
+
 // Groovy Engine: 2.1.7
-println 'Groovy Engine: ' + groovy.lang.GroovySystem.version
+println 'Groovy: ' + groovy.lang.GroovySystem.getVersion()
 
 // Java(TM) SE Runtime Environment 1.6.0_30 (headless)
 println 'JRE: ' + Settings.getJavaRuntimeIdentifier()
 
 // 32-bit Java HotSpot(TM) Client VM
-println String.format('JVM: %d-bit %s', com.sun.jna.Platform.is64Bit() ? 64 : 32, _system['java.vm.name'])
+println String.format('JVM: %d-bit %s', com.sun.jna.Platform.is64Bit() ? 64 : 32, System.getProperty('java.vm.name'))
 
 // CPU/MEM: 4 Core / 1 GB Max Memory / 15 MB Used Memory
 println String.format('CPU/MEM: %s Core / %s Max Memory / %s Used Memory', Runtime.runtime.availableProcessors(), org.apache.commons.io.FileUtils.byteCountToDisplaySize(Runtime.runtime.maxMemory()), org.apache.commons.io.FileUtils.byteCountToDisplaySize(Runtime.runtime.totalMemory() - Runtime.runtime.freeMemory()))
 
 // Windows 7 (x86)
-println String.format('OS: %s (%s)', _system['os.name'], _system['os.arch'])
+println String.format('OS: %s (%s)', System.getProperty('os.name'), System.getProperty('os.arch'))
 
 // MAS
 println 'Package: ' + Settings.getApplicationDeployment().toUpperCase()
+
+// Application Data folder
 println 'Data: ' + Settings.getApplicationFolder()
 
 // print uname -a if available
 try {
-	println String.format('uname: %s', ['uname', '-a'].execute().text.trim())
+	print 'uname: '
+	def uname = ['uname', '-a'].execute().text.trim()
+	println uname
 } catch(Throwable error) {
 	// ignore
 }
