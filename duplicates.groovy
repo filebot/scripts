04a646ced4e4741ae3fa51cb60c174cc1ab7a7b8
rednoah <rednoah@users.sourceforge.net>
2016-10-24 22:57:09 +0800

    Fix duplicates script (use correct video quality order)

--- duplicates.groovy
+++ duplicates.groovy
@@ -1,19 +1,21 @@
 // filebot -script fn:duplicates --action delete /path/to/media
 
+
+def delete = 'DELETE'.equalsIgnoreCase(_args.action)
+
+
 args.getFiles{ it.isVideo() }.groupBy{ it.metadata }.each{ m, fs ->
 	if (m && fs.size() > 1) {
 		log.info "[*] $m"
 
-		fs.toSorted(new VideoQuality()).eachWithIndex{ f, i ->
+		fs.toSorted(new VideoQuality().reversed()).eachWithIndex{ f, i ->
 			if (i == 0) {
-				log.fine "[+] 1. $f"
+				log.finest "[+] 1. $f"
+			} else if (delete) {
+				log.warning "[DELETE] ${i+1}. $f"
+				Files.delete(f.toPath())
 			} else {
 				log.fine "[-] ${i+1}. $f"
-
-				if ('delete' == _args.action) {
-					log.info "[DELETE] $f"
-					Files.delete(f.toPath())
-				}
 			}
 		}
 	}
