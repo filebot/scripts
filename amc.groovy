382a4804b8645b43adfa22d1a970cc21a070b909
rednoah <rednoah@users.sourceforge.net>
2017-03-21 03:29:24 +0800

    HorribleSubs does not adhere to AniDB numbering (default to TheTVDB instead)
    https://www.filebot.net/forums/viewtopic.php?f=8&t=4850

--- amc.groovy
+++ amc.groovy
@@ -66,7 +66,7 @@ def forceSeries(f) {
 }
 
 def forceAnime(f) {
-	label =~ /^(?i:Anime)/ || f.dir.listPath().any{ it.name ==~ /(?i:Anime)/ } || (f.isVideo() && (f.name =~ /(?i:HorribleSubs)/ || f.name =~ /[\(\[]\p{XDigit}{8}[\]\)]/ || any{ getMediaInfo(f, '{media.AudioLanguageList} {media.TextCodecList}').tokenize().containsAll(['Japanese', 'ASS']) && (parseEpisodeNumber(f.name, false) != null || getMediaInfo(f, '{minutes}').toInteger() < 60) }{ false }))
+	label =~ /^(?i:Anime)/ || f.dir.listPath().any{ it.name ==~ /(?i:Anime)/ } || (f.isVideo() && (f.name =~ /[\(\[]\p{XDigit}{8}[\]\)]/ || any{ getMediaInfo(f, '{media.AudioLanguageList} {media.TextCodecList}').tokenize().containsAll(['Japanese', 'ASS']) && (parseEpisodeNumber(f.name, false) != null || getMediaInfo(f, '{minutes}').toInteger() < 60) }{ false }))
 }
 
 def forceAudio(f) {
