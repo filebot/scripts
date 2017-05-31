25f1abe12486bbec5eec5b5e39b7ec0abb23adf3
rednoah <rednoah@users.sourceforge.net>
2017-05-31 10:39:22 +0800

    Use FileBot logging instead of STDIO

--- xattr.groovy
+++ xattr.groovy
@@ -2,26 +2,16 @@
 
 
 args.getFiles{ f -> f.xattr.size() > 0 }.each{ f ->
-	println f
+	log.finest "$f"
 
 	f.xattr.each{ k, v ->
-		println "\t$k: $v"
-
-		// auto-update pre-v41 xattr keys
-		if (k in ['metadata', 'filename']) {
-			def newk = "net.filebot.$k"
-			if (f.xattr[newk] == null) {
-				// UPDATE 
-				println "\t\tIMPORT xattr $k => $newk"
-				f.xattr[newk] = v.replace("net.sourceforge.filebot", "net.filebot")
-			}
-		}
+		log.info "\t$k: $v"
 	}
 
 	// clear xattr mode
 	if (_args.action == 'clear') {
 		f.xattr.clear()
-		println '*** CLEARED ***'
+		log.finest '*** CLEARED ***'
 	}
 
 	// import xattr metadata into Mac OS X Finder tags (UAYOR)
@@ -40,7 +30,7 @@ args.getFiles{ f -> f.xattr.size() > 0 }.each{ f ->
 			}
 		}
 
-		println "*** Write tag plist to xattr [$xkey]: $tags ***"
+		log.info "*** Write tag plist to xattr [$xkey]: $tags ***"
 		f.xattr[xkey] = plist
 	}
 }
