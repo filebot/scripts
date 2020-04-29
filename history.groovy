617b3eb57006a2d0add5b69a392ca49ecdfbf43d
rednoah <rednoah@users.sourceforge.net>
2020-04-29 20:24:25 +0700

    Additional sanity checks and warnings for users that call history with an empty history

--- history.groovy
+++ history.groovy
@@ -5,13 +5,23 @@
 def format = _args.format ?: '''${from}\t${to}'''
 def template = new groovy.text.GStringTemplateEngine().createTemplate(format)
 
+
 // use args to list history only for the given folders if desired
 def accept(from, to) {
 	return args.empty || (args.any{ to.absolutePath.startsWith(it.absolutePath) } && to.exists())
 }
 
 
-getPersistentRenameLog().each { from, to ->
-	if (accept(from, to))
+// read history file
+def history = getPersistentRenameLog()
+
+// sanity check
+if (history.empty) {
+	die "No History", ExitCode.NOOP
+}
+
+history.each{ from, to ->
+	if (accept(from, to)) {
 		println template.make(from:from, to:to)
+	}
 }
