a43edd75cd590eea658ccf967d33d90c8be62145
rednoah <rednoah@users.sourceforge.net>
2020-12-06 04:03:03 +0700

    Remove GStringTemplateEngine

--- history.groovy
+++ history.groovy
@@ -1,11 +1,6 @@
 #!/usr/bin/env filebot -script
 
 
-// use --format parameter to specify your own output format
-def format = _args.format ?: '''${from}\t${to}'''
-def template = new groovy.text.GStringTemplateEngine().createTemplate(format)
-
-
 // use args to list history only for the given folders if desired
 def accept(from, to) {
 	return args.empty || (args.any{ to.absolutePath.startsWith(it.absolutePath) } && to.exists())
@@ -22,6 +17,6 @@ if (history.empty) {
 
 history.each{ from, to ->
 	if (accept(from, to)) {
-		println template.make(from: from, to: to)
+		println "${from}\t${to}"
 	}
 }
