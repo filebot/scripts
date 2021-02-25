737c7f4c30941790d9934fc4845bd582d865e3ec
Tyler Forest-Hauser <tyler@foresthauser.com>
2021-02-25 19:54:08 +0100

    Customize Discord notifications • Does not include Sonarr/Radarr ids yet

--- lib/web.groovy
+++ lib/web.groovy
@@ -1,186 +1,187 @@
 /****************************************************************************
  * E-Mail via SMTP
- * 				https://en.wikipedia.org/wiki/Simple_Mail_Transfer_Protocol
+ *                 https://en.wikipedia.org/wiki/Simple_Mail_Transfer_Protocol
  ****************************************************************************/
 def Gmail(username, password) {
-	new EmailClient(host: 'smtp.gmail.com', port: 587, username: username, password: password)
+    new EmailClient(host: 'smtp.gmail.com', port: 587, username: username, password: password)
 }
 
 def Email(host, port, username, password) {
-	new EmailClient(host: host, port: port as int, username: username, password: password)
+    new EmailClient(host: host, port: port as int, username: username, password: password)
 }
 
 class EmailClient {
-	def username
-	def password
-	def host
-	def port
 
-	def sendHtml = { from, to, subject, message ->
-		// create email message
-		def email = new org.apache.commons.mail.HtmlEmail()
-		email.setFrom(from, 'FileBot')
-		email.addTo(to)
+    def username
+    def password
+    def host
+    def port
 
-		// set html message body
-		email.setSubject(subject)
-		email.setHtmlMsg(message)
+    def sendHtml = { from, to, subject, message ->
+        // create email message
+        def email = new org.apache.commons.mail.HtmlEmail()
+        email.setFrom(from, 'FileBot')
+        email.addTo(to)
 
-		// configure SMTP properties
-		email.setHostName(host)
-		email.setSmtpPort(port)
+        // set html message body
+        email.setSubject(subject)
+        email.setHtmlMsg(message)
 
-		if (username && password) {
-			email.setAuthentication(username, password)
-			email.setStartTLSEnabled(true)
-		}
+        // configure SMTP properties
+        email.setHostName(host)
+        email.setSmtpPort(port)
 
-		// send email
-		try {
-			email.send()
-		} catch(org.apache.commons.mail.EmailException e) {
-			// use root cause as error message
-			throw e.cause ?: e
-		}
-	}
+        if (username && password) {
+            email.setAuthentication(username, password)
+            email.setStartTLSEnabled(true)
+        }
+
+        // send email
+        try {
+            email.send()
+        } catch (org.apache.commons.mail.EmailException e) {
+            // use root cause as error message
+            throw e.cause ?: e
+        }
+    }
+
 }
-
-
-
 
 /****************************************************************************
  * Pushover
- * 				https://pushover.net
+ *                 https://pushover.net
  ****************************************************************************/
 def Pushover(user, token) {
-	new PushoverClient(user:user, token:token)
+    new PushoverClient(user:user, token:token)
 }
 
 class PushoverClient {
-	def user
-	def token
 
-	def endpoint = new URL('https://api.pushover.net/1/messages.xml')
-	def titleLimit = 100
-	def messageLimit = 512
+    def user
+    def token
 
-	def send = { title, message ->
-		def lines = message.readLines().collect{ it + '\n' }
-		def pages = []
+    def endpoint = new URL('https://api.pushover.net/1/messages.xml')
+    def titleLimit = 100
+    def messageLimit = 512
 
-		def currentPage = ''
-		lines.each{ line ->
-			if (currentPage.length() + line.length() >= messageLimit) {
-				pages << currentPage
-				currentPage = ''
-			}
-			currentPage += line
-		}
-		pages << currentPage
+    def send = { title, message ->
+        def lines = message.readLines().collect { it + '\n' }
+        def pages = []
 
-		// use title and make space for pagination
-		def pageTitle = (title as String)
+        def currentPage = ''
+        lines.each { line ->
+            if (currentPage.length() + line.length() >= messageLimit) {
+                pages << currentPage
+                currentPage = ''
+            }
+            currentPage += line
+        }
+        pages << currentPage
 
-		// submit post requests
-		pages = pages.findAll{ it.length() > 0 }
+        // use title and make space for pagination
+        def pageTitle = (title as String)
 
-		pages.eachWithIndex(){ m, i ->
-			if (i > 0) sleep(1000) // max 1 request / sec
-			def t = pages.size() <= 1 ? pageTitle : "${pageTitle} (${i+1}/${pages.size()})"
-			post(m, [title: t])
-		}
-	}
+        // submit post requests
+        pages = pages.findAll { it.length() > 0 }
 
-	def post = { text, parameters = [:] ->
-		// inject default post parameters
-		parameters << [token:token, user:user, message:text as String]
+        pages.eachWithIndex() { m, i ->
+            if (i > 0) sleep(1000) // max 1 request / sec
+            def t = pages.size() <= 1 ? pageTitle : "${pageTitle} (${i+1}/${pages.size()})"
+            post(m, [title: t])
+        }
+    }
 
-		// post and process response
-		endpoint.post(parameters, [:])
-	}
+    def post = { text, parameters = [:] ->
+        // inject default post parameters
+        parameters << [token:token, user:user, message:text as String]
+
+        // post and process response
+        endpoint.post(parameters, [:])
+    }
+
 }
-
-
-
 
 /****************************************************************************
  * PushBullet
- * 				https://www.pushbullet.com
+ *                 https://www.pushbullet.com
  ****************************************************************************/
 def PushBullet(apikey) {
-	new PushBulletClient(apikey:apikey)
+    new PushBulletClient(apikey:apikey)
 }
 
 class PushBulletClient {
-	def apikey
 
-	def endpoint_pushes = new URL('https://api.pushbullet.com/v2/pushes')
-	def endpoint_upload = new URL('https://api.pushbullet.com/v2/upload-request')
+    def apikey
 
-	def sendFile = { file_name, file_content, file_type, body = null, email = null ->
-		def requestProperties = [Authorization: 'Basic '+apikey.getBytes().encodeBase64()]
+    def endpoint_pushes = new URL('https://api.pushbullet.com/v2/pushes')
+    def endpoint_upload = new URL('https://api.pushbullet.com/v2/upload-request')
 
-		// prepare upload
-		def uploadRequestData = [file_name: file_name, file_type: file_type]
-		def response = new JsonSlurper().parseText(endpoint_upload.post(uploadRequestData, requestProperties).text)
+    def sendFile = { file_name, file_content, file_type, body = null, email = null ->
+        def requestProperties = [Authorization: 'Basic ' + apikey.getBytes().encodeBase64()]
 
-		// build multipart/form-data -- the most horrible data format that we will never get rid off :(
-		def MBD = '--'		// random -- before the boundry, or after, or not at all (there goes 4 hours of my life)
-		def EOL = '\r\n'	// CR+NL required per spec?! I shit you not!
+        // prepare upload
+        def uploadRequestData = [file_name: file_name, file_type: file_type]
+        def response = new JsonSlurper().parseText(endpoint_upload.post(uploadRequestData, requestProperties).text)
 
-		def multiPartFormBoundary = '----------NCC-1701-E'
-		def multiPartFormType = 'multipart/form-data; boundary='+multiPartFormBoundary
-		def multiPartFormData = new ByteArrayOutputStream()
+        // build multipart/form-data -- the most horrible data format that we will never get rid off :(
+        def MBD = '--'        // random -- before the boundry, or after, or not at all (there goes 4 hours of my life)
+        def EOL = '\r\n'    // CR+NL required per spec?! I shit you not!
 
-		multiPartFormData.withWriter('UTF-8') { out ->
-			response.data.each{ key, value -> 
-				out << MBD << multiPartFormBoundary << EOL
-				out << 'Content-Disposition: form-data; name="' << key << '"' << EOL << EOL
-				out << value << EOL
-			}
-			out << MBD << multiPartFormBoundary << EOL
-			out << 'Content-Disposition: form-data; name="file"; filename="' << file_name << '"' << EOL
-			out << 'Content-Type: ' << file_type << EOL << EOL
-			out << file_content << EOL
-			out << MBD << multiPartFormBoundary << MBD << EOL
-		}
+        def multiPartFormBoundary = '----------NCC-1701-E'
+        def multiPartFormType = 'multipart/form-data; boundary=' + multiPartFormBoundary
+        def multiPartFormData = new ByteArrayOutputStream()
 
-		// do upload to Amazon S3
-		new URL(response.upload_url).post(multiPartFormData.toByteArray(), multiPartFormType, ['Content-Encoding':'gzip'])
+        multiPartFormData.withWriter('UTF-8') { out ->
+            response.data.each { key, value ->
+                out << MBD << multiPartFormBoundary << EOL
+                out << 'Content-Disposition: form-data; name="' << key << '"' << EOL << EOL
+                out << value << EOL
+            }
+            out << MBD << multiPartFormBoundary << EOL
+            out << 'Content-Disposition: form-data; name="file"; filename="' << file_name << '"' << EOL
+            out << 'Content-Type: ' << file_type << EOL << EOL
+            out << file_content << EOL
+            out << MBD << multiPartFormBoundary << MBD << EOL
+        }
 
-		// push file link
-		def pushFileData = [type: 'file', file_url: response.file_url, file_name: file_name, file_type: file_type, body: body, email: email]
-		endpoint_pushes.post(pushFileData, requestProperties)
-	}
+        // do upload to Amazon S3
+        new URL(response.upload_url).post(multiPartFormData.toByteArray(), multiPartFormType, ['Content-Encoding':'gzip'])
+
+        // push file link
+        def pushFileData = [type: 'file', file_url: response.file_url, file_name: file_name, file_type: file_type, body: body, email: email]
+        endpoint_pushes.post(pushFileData, requestProperties)
+    }
+
 }
-
-
-
 
 /****************************************************************************
  * Discord
- * 				https://discord.com
+ *                 https://discord.com
  ****************************************************************************/
 def Discord(webhook) {
-	new DiscordClient(webhook:webhook)
+    new DiscordClient(webhook:webhook)
 }
 
 class DiscordClient {
-	def webhook
 
-	def postRenameMap = { subject, map ->
-		// use custom discord embeds since attachments do not play well with smart phones (i.e. Content-Disposition: attachment)
-		def json = [
-			content: "FileBot finished processing **${subject}** (${map.size()} files).",
-			embeds: map.collect{ from, to -> [to.parentFile, from.name, to.name] }.groupBy{ it[0] }.take(9).collect{ group, names ->
-				[
-					"title": group.getStructurePathTail().toString().take(256),
-					"fields": names.take(25).collect{ parent, from, to -> ["name": from.take(256), "value": to.take(1024), inline: true] },
-					"color": new Random().nextInt(0xFFFFFF)
-				]
-			} + ["footer": [text: "Generated by ${Settings.applicationIdentifier} on ${InetAddress.localHost.hostName} at ${new Date()}", icon_url: 'https://app.filebot.net/avatar.png']]
-		]
-		def data = JsonOutput.toJson(json)
-		new URL(webhook).post(data.getBytes('UTF-8'), 'application/json', [:])
-	}
+    def webhook
+
+    def postRenameMap = { subject, map ->
+        // use custom discord embeds since attachments do not play well with smart phones (i.e. Content-Disposition: attachment)
+        def json = [
+            content: ' ',
+            embeds: map.collect { from, to -> [to.parentFile, from.name, to.name] }.groupBy { it[0] }.take(9).collect { group, names ->
+                [
+                    'title': "[ RENAME ] - **${subject}** - ${map.size()} ${(map.size() == 1) ? 'file' : 'files'}",
+                    'description': group.getStructurePathTail().toString().take(256),
+                    'fields': names.take(25).collect { parent, from, to -> ['name': from.take(256), 'value': to.take(1024), inline: true] },
+                    'color': 15298845,
+					'footer': [text: 'Thanks to FileBot', icon_url: 'https://app.filebot.net/avatar.png']
+                ]
+            }
+        ]
+        def data = JsonOutput.toJson(json)
+        new URL(webhook).post(data.getBytes('UTF-8'), 'application/json', [:])
+}
+
 }
