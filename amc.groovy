c23dbedcdbcae127d076d8c4852ff26643483c0e
rednoah <rednoah@users.sourceforge.net>
2021-11-29 08:56:16 +0700

    Print xattr lines with warning highlight color to make it more clear that the files being processed have already been processed and tagged before

--- amc.groovy
+++ amc.groovy
@@ -310,7 +310,7 @@ if (excludeList && !testRun) {
 input.each{ f -> log.fine "Input: $f" }
 
 // print xattr metadata
-input.each{ f -> if (f.metadata) log.finest "xattr: [$f.name] => [$f.metadata]" }
+input.each{ f -> if (f.metadata) log.warning "xattr: [$f.name] => [$f.metadata]" }
 
 // early abort if there is nothing to do
 if (input.size() == 0) {
