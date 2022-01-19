b2d517440a72fb8b239c9b01eff564340b7db90b
rednoah <rednoah@filebot.net>
2022-01-19 10:14:42 +0700

    Sorry. OpenSubtitles Login now requires FileBot 4.9.5 (r9072) or higher due to server-side API changes.

--- configure.groovy
+++ configure.groovy
@@ -4,15 +4,26 @@
 osdbUser = any{ osdbUser }{ console.printf('Enter OpenSubtitles username: '); console.readLine() }
 osdbPwd = any{ osdbPwd }{ console.printf('Enter OpenSubtitles password: '); console.readLine() }
 
+
 /* --------------------------------------------------------------------- */
 
-if (osdbUser) {
-	console.printf('Testing OpenSubtitles... ')
-	WebServices.setLogin(WebServices.LOGIN_OPENSUBTITLES, osdbUser, osdbPwd)
-	WebServices.OpenSubtitles.login()
+
+if (Settings.getApplicationRevisionNumber() < 9072) {
+	die "Sorry. OpenSubtitles Login now requires FileBot 4.9.5 (r9072) or higher due to server-side API changes."
+}
+
+
+// set login
+if (osdbUser && osdbPwd) {
+	console.printf('Testing OpenSubtitles login details... ')
+	WebServices.setLogin(WebServices.OpenSubtitles, osdbUser, osdbPwd)
 	console.printf('OK\n')
-} else if (osdbUser.empty && osdbPwd.empty) {
+}
+
+
+// clear login
+if (osdbUser.empty && osdbPwd.empty) {
 	console.printf('Clear OpenSubtitles login details... ')
-	WebServices.setLogin(WebServices.LOGIN_OPENSUBTITLES, null, null)
+	WebServices.setLogin(WebServices.OpenSubtitles, null, null)
 	console.printf('OK\n')
 }
