87194c8665e70741354a19dfd6fecdfd66b59504
rednoah <rednoah@filebot.net>
2022-02-03 09:10:06 +0700

    Generate Episode NFO (MultiEpisode support)

--- nfo.groovy
+++ nfo.groovy
@@ -2,68 +2,72 @@
 
 
 def fetchEpisodeNfo(outputFile, episodeObject, episodeFile) {
-	def si = episodeObject.seriesInfo
-	def ei = episodeObject.info
 	def mi = MediaInfo.snapshot(episodeFile)
+	def si = episodeObject.seriesInfo
 
 	def xml = XML {
-		episodedetails {
-			if (si.database =~ 'TheTVDB') {
-				uniqueid(type:'tvdb', ei.id)
-			}
-			if (si.database =~ 'TheMovieDB') {
-				uniqueid(type:'tmdb', ei.id)
-			}
+		episodeObject.each{ episodePart ->
+			// retrieve episode information for each episode or multi-episode component
+			def ei = episodePart.info
 
-			title(ei.title)
-			showtitle(si.name)
-
-			season(ei.season)
-			episode(ei.episode)
-			aired(ei.airdate)
-			plot(ei.overview)
-			runtime(si.runtime)
-			thumb(aspect:'thumb', ei.image)
-
-			ei.people.each { p ->
-				if (p.director) {
-					director(p.name)
-				} else if (p.writer) {
-					credits(p.name)
-				} else if (p.actor) { 
-					actor {
-						name(p.name)
-						role(p.character)
-						order(p.order)
-						thumb(p.image)
-					}
-				} else if (p.department == 'Writing') {
-					credits("$p.name ($p.job)")
+			episodedetails {
+				if (si.database =~ 'TheTVDB') {
+					uniqueid(type:'tvdb', ei.id)
 				}
-			}
+				if (si.database =~ 'TheMovieDB') {
+					uniqueid(type:'tmdb', ei.id)
+				}
 
-			fileinfo {
-				streamdetails {
-					mi.each { kind, streams ->
-						def section = kind.toString().toLowerCase()
-						streams.each { s ->
-							if (section == 'video') {
-								video {
-									codec((s.'Encoded_Library/Name' ?: s.'CodecID/Hint' ?: s.'Format').replaceAll(/[ ].+/, '').trim())
-									aspect(s.'DisplayAspectRatio')
-									width(s.'Width')
-									height(s.'Height')
+				title(ei.title)
+				showtitle(si.name)
+
+				season(ei.season)
+				episode(ei.episode)
+				aired(ei.airdate)
+				plot(ei.overview)
+				runtime(si.runtime)
+				thumb(aspect:'thumb', ei.image)
+
+				ei.people.each { p ->
+					if (p.director) {
+						director(p.name)
+					} else if (p.writer) {
+						credits(p.name)
+					} else if (p.actor) { 
+						actor {
+							name(p.name)
+							role(p.character)
+							order(p.order)
+							thumb(p.image)
+						}
+					} else if (p.department == 'Writing') {
+						credits("$p.name ($p.job)")
+					}
+				}
+
+				fileinfo {
+					streamdetails {
+						mi.each { kind, streams ->
+							def section = kind.toString().toLowerCase()
+							streams.each { s ->
+								if (section == 'video') {
+									video {
+										codec((s.'Encoded_Library/Name' ?: s.'CodecID/Hint' ?: s.'Format').replaceAll(/[ ].+/, '').trim())
+										aspect(s.'DisplayAspectRatio')
+										width(s.'Width')
+										height(s.'Height')
+									}
 								}
-							}
-							if (section == 'audio') {
-								audio {
-									codec((s.'CodecID/Hint' ?: s.'Format').replaceAll(/\p{Punct}/, '').trim())
-									language(s.'Language/String3')
-									channels(s.'Channel(s)_Original' ?: s.'Channel(s)')
+								if (section == 'audio') {
+									audio {
+										codec((s.'CodecID/Hint' ?: s.'Format').replaceAll(/\p{Punct}/, '').trim())
+										language(s.'Language/String3')
+										channels(s.'Channel(s)_Original' ?: s.'Channel(s)')
+									}
 								}
-							}
-							if (section == 'text') {
-								subtitle { language(s.'Language/String3') }
+								if (section == 'text') {
+									subtitle { language(s.'Language/String3') }
+								}
 							}
 						}
 					}
@@ -76,14 +80,15 @@ def fetchEpisodeNfo(outputFile, episodeObject, episodeFile) {
 	xml.saveAs(outputFile)
 
 	// write episode thumbnail to file
-	if (ei.image) {
-		def thumbnailFile = outputFile.dir / outputFile.nameWithoutExtension + '.png'
-		if (thumbnailFile.exists()) {
-			log.finest "Skip [$ei.image] because [$thumbnailFile.name] already exists"
-		} else {
-			log.fine "Fetch $thumbnailFile [$ei.image]"
-			ei.image.fetch().getImage().saveAs(thumbnailFile)
-		}
+	def thumbnailFile = outputFile.dir / outputFile.nameWithoutExtension + '.jpg'
+	if (thumbnailFile.exists()) {
+		return
+	}
+
+	def thumbnailUrl = episodeObject.info.image
+	if (thumbnailUrl) {
+		log.fine "Fetch $thumbnailFile [$thumbnailUrl]"
+		thumbnailUrl.saveAs(thumbnailFile)
 	}
 }
 
@@ -97,10 +102,11 @@ args.files.each{ f ->
 			def nfoFile = f.dir / f.nameWithoutExtension + '.nfo'
 			if (nfoFile.exists()) {
 				log.finest "Skip [$f.name] because [$nfoFile.name] already exists"
-			} else {
-				log.info "Generate Episode NFO: $m [$nfoFile]"
-				fetchEpisodeNfo(nfoFile, m, f)
+				return
 			}
-		}		
+
+			log.info "Generate Episode NFO: $m [$nfoFile]"
+			fetchEpisodeNfo(nfoFile, m, f)
+		}
 	}
 }
