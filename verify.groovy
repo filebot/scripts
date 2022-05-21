05cfbb8e70266ded33e0f3eebb6d69c043c99eb1
rednoah <rednoah@filebot.net>
2022-05-21 22:24:38 +0200

    Support different hash types via --mode parameter, e.g. --mode md5

--- verify.groovy
+++ verify.groovy
@@ -1,28 +1,31 @@
 #!/usr/bin/env filebot -script
 
 
+def hashType = any{ _args.mode.toUpperCase() }{ 'CRC32' }
+
+
 parallel(args.files.collect{ f ->
 	return {
-		def attr_hash = f.xattr['CRC32']
-		def calc_hash = f.hash('crc32')
+		def attr_hash = f.xattr[hashType]
+		def calc_hash = f.hash(hashType)
 
 		if (attr_hash) {
 			log.finest "$attr_hash $calc_hash $f"
 
-			// abort if a CRC32 mismatch has been found
+			// abort if a mismatch has been found
 			if (attr_hash != calc_hash) {
-				die "CRC32 mismatch: $attr_hash does not match: $calc_hash $f"
+				die "$hashType mismatch: $attr_hash does not match: $calc_hash $f"
 			}
 		} else {
 			// compute checksum if it cannot be read from xattr
-			log.warning "Set CRC32 xattr: $calc_hash $f"
+			log.warning "Set $hashType xattr: $calc_hash $f"
 
-			f.xattr['CRC32'] = calc_hash
-			f.xattr['CRC32.mtime'] = f.lastModified() as String
+			f.xattr[hashType] = calc_hash
+			f.xattr[hashType + '.mtime'] = f.lastModified() as String
 
 			// verify that xattr has been set correctly
-			if (f.xattr['CRC32'] != calc_hash) {
-				die "Failed to set CRC32 xattr: $f"
+			if (f.xattr[hashType] != calc_hash) {
+				die "Failed to set $hashType xattr: $f"
 			}
 		}
 	}
