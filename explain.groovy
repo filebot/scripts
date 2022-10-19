ecba7b497bd1efa45dad4ccca642a79fcbdfcbd1
Reinhard Pointner <rednoah@filebot.net>
2022-10-19 14:07:01 +0700

    Print local index matches (if the --q option is passed along)

--- explain.groovy
+++ explain.groovy
@@ -1,17 +1,51 @@
 #!/usr/bin/env filebot -script
 
 
-args.getFiles{ it.isVideo() }.each{ f ->
-	println ' File / Object / MediaInfo '.center(80, '-')
 
-	println 'File:    ' + f
-	println 'Object:  ' + f.xattr['net.filebot.metadata']
-	println 'Media:   ' + any{ MediaInfo.snapshot(f) }{ null }
+/*
+ * Print match metrics (if a file argument is passed along)
+ */
+if (args) {
+	args.getFiles{ it.isVideo() }.each{ f ->
+		println ' File / Object / MediaInfo '.center(80, '-')
 
-	if (f.metadata) {
-		println ' Episode Metrics '.center(80, '-')
-		EpisodeMetrics.defaultSequence(false).each{ m ->
-			println String.format('%-20s % 1.1f', m, m.getSimilarity(f, f.metadata))
+		println 'File:    ' + f
+		println 'Object:  ' + f.xattr['net.filebot.metadata']
+		println 'Media:   ' + any{ MediaInfo.snapshot(f) }{ null }
+
+		if (f.metadata) {
+			println ' Episode Metrics '.center(80, '-')
+			EpisodeMetrics.defaultSequence(false).each{ m ->
+				println String.format('%-20s % 1.1f', m, m.getSimilarity(f, f.metadata))
+			}
+		}
+	}
+}
+
+
+
+/*
+ * Print local index matches (if the --q option is passed along)
+ */
+if (_args.query) {
+	// search series index
+	WebServices.episodeListProviders.each{ db ->
+		db.index.each{ s ->
+			s.effectiveNames.each{ n ->
+				if (n.findWordMatch(_args.query)) {
+					println "$n [$db.identifier::$s.id]"
+				}
+			}
+		}
+	}
+	// search movie index
+	WebServices.movieLookupServices.each{ db ->
+		db.index.each{ m ->
+			m.effectiveNames.each{ n ->
+				if (n.findWordMatch(_args.query)) {
+					println "$n [$db.identifier::$m.id]"
+				}
+			}
 		}
 	}
 }
