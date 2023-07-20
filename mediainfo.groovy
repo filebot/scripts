6e978dc22d3efd877ecaa17a7c4bb98590737415
Reinhard Pointner <rednoah@filebot.net>
2023-07-21 03:19:24 +0700

    Use FileBot 5.* API

--- mediainfo.groovy
+++ mediainfo.groovy
@@ -1,16 +1,15 @@
 #!/usr/bin/env filebot -script
 
-log.finest "# ${MediaInfo.version()}"
-
 args.files.each{ f ->
 	log.fine "\n[$f]"
-	MediaInfo.snapshot(f).each{ kind, streams ->
-		// find optimal padding
-		def pad = streams*.keySet().flatten().collect{ it.length() }.max()
 
-		streams.each{ p -> 
-			log.finest "\n[$kind]"
-			p.each{ k,v -> 
+	f.mediaInfo.each{ kind ->
+		kind.each{ stream -> 
+			log.finest "\n${stream.StreamKind} #${stream.StreamKindID}"
+
+			// find optimal padding
+			def pad = stream.keySet().flatten().collect{ it.length() }.max()
+			stream.each{ k,v -> 
 				log.info "${k.padRight(pad)} : $v"
 			}
 		}
