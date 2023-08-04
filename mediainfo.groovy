fcfcf92f3342d1e73eecbb05ad483c647ef67e89
Reinhard Pointner <rednoah@filebot.net>
2023-08-04 21:44:07 +0700

    Print raw output

--- mediainfo.groovy
+++ mediainfo.groovy
@@ -17,7 +17,7 @@ if (_args.mode == /raw/) {
 			// write xattr
 			if (raw) {
 				f.xattr['net.filebot.mediainfo'] = raw
-				f.xattr['net.filebot.mediainfo.mtime'] = f.lastModified() as String	
+				f.xattr['net.filebot.mediainfo.mtime'] = f.lastModified() as String
 			}
 		}
 	}
