9ba94671fe7530f039a0ed88e19f0ef56d6fb610
Reinhard Pointner <rednoah@filebot.net>
2024-05-05 00:28:05 +0200

    print stat for each valid file argument as well (Unix only)

--- sysenv.groovy
+++ sysenv.groovy
@@ -4,6 +4,7 @@
 log.fine '\n# Local Time #'
 log.info "$now"
 
+
 log.fine '\n# Process Tree #'
 try {
 	def t = []
@@ -17,17 +18,37 @@ try {
 	log.warning "$e"
 }
 
+
 log.fine '\n# Environment Variables #'
 _environment.toSorted{ it.key }.each{ k, v ->
 	log.info "$k: $v"
 }
+
 
 log.fine '\n# Java System Properties #'
 _system.toSorted{ it.key }.each{ k, v ->
 	log.info "$k: $v"
 }
 
+
 log.fine '\n# Arguments #'
 _args.argumentArray.eachWithIndex{ a, i ->
 	log.info "args[$i] = $a"
+}
+
+
+// print stat for each valid file argument as well (Unix only)
+if (File.separator == '/') {
+	args.each{ f ->
+		if (f.isFile()) {
+			log.fine "\n# ${f} #"
+			['unix:dev', 'unix:ino', 'unix:nlink', 'unix:lastModifiedTime', 'unix:uid', 'unix:gid', 'unix:owner', 'unix:group', 'unix:permissions'].each{ k ->
+				try {
+					log.info "$k = ${f.getAttribute(k)}"
+				} catch(Throwable e) {
+					log.info "$k = $e"
+				}
+			}
+		}
+	}
 }
