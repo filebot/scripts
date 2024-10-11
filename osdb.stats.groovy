71eb4fd84c1a655274ec9a99e4375fea51c0ddb5
Reinhard Pointner <rednoah@filebot.net>
2024-10-12 00:10:46 +0700

    Ensure that login errors are not silently ignored
    e.g.
    java.util.concurrent.CancellationException: https://api.opensubtitles.com/api/v1/login [401 Unauthorized Request]

--- osdb.stats.groovy
+++ osdb.stats.groovy
@@ -1,15 +1,16 @@
 #!/usr/bin/env -S filebot -script
 
 
-// e.g. [users_online_program:2483, xmlrpc_url:http://api.opensubtitles.org/xml-rpc, application:OpenSuber v0.2, download_limits:[limit_check_by:user_id, global_24h_download_limit:200, client_24h_download_count:0, client_download_quota:200, user_id:607030, client_24h_download_limit:200, client_ip:140.112.25.51], subs_downloads:1183798660, subs_subtitle_files:2621058, contact:admin@opensubtitles.org, total_subtitles_languages:67, users_online_total:3600, users_max_alltime:27449, xmlrpc_version:0.1, movies_total:164709, seconds:0.006, users_registered:1114040, usersloggedin:27, website_url:http://www.opensubtitles.org, movies_aka:421878]
-def info = WebServices.OpenSubtitles.getServerInfo()
-
-if (WebServices.OpenSubtitles.class =~ /XmlRpc/) {
-	info.download_limits.each{ k, v ->
-		println "${k} = ${v}"
+try {
+	if (WebServices.OpenSubtitles.class =~ /XmlRpc/) {
+		WebServices.OpenSubtitles.getServerInfo().download_limits.each{ k, v ->
+			println "${k} = ${v}"
+		}
+	} else {
+		WebServices.OpenSubtitles.getServerInfo().each{ k, v ->
+			println "${k} = ${v}"
+		}
 	}
-} else {
-	info.each{ k, v ->
-		println "${k} = ${v}"
-	}
+} catch(e) {
+	log.severe e.message
 }
