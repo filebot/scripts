e057bc66ecc251bb745e7511c8cc1f3a5417709d
Reinhard Pointner <rednoah@filebot.net>
2025-02-04 20:58:57 +0700

    Rename Folder by Folder script

--- rename.groovy
+++ rename.groovy
@@ -3,9 +3,11 @@
 args.eachMediaFolder{ f ->
 	if (f.disk) {
 		// process a disk folder as a single unit
+		help "Disk Folder: $f"
 		rename(file: f)
 	} else {
 		// process a set of files as usual
+		help "Media Folder: $f"
 		rename(folder: f)
 	}
 	// store history
