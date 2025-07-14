639fb17c2745a08c66089915840b42af2fcdd701
Reinhard Pointner <rednoah@filebot.net>
2025-07-14 15:53:36 +0700

    Improved support for TheTVDBv4 artwork

--- artwork.groovy
+++ artwork.groovy
@@ -2,24 +2,29 @@
 
 
 def fetchMovieArtwork(movie, language, movieFolder) {
-	fetchArtwork(movie, 'posters', language, movieFolder / 'poster.jpg')
-	fetchArtwork(movie, 'backdrops', null, movieFolder / 'backdrop.jpg')
+	fetchArtwork(movie, 'posters', language, [], movieFolder / 'poster.jpg')
+	fetchArtwork(movie, 'backdrops', null, [], movieFolder / 'backdrop.jpg')
 }
 
 
-def fetchSeriesArtwork(series, language, seriesFolder, seasonFolder) {
+def fetchSeriesArtwork(series, language, season, seriesFolder, seasonFolder) {
 	if (series.database == 'TheTVDB') {
-		fetchArtwork(series, 'poster', language, seriesFolder / 'poster.jpg')
-		fetchArtwork(series, 'series', language, seriesFolder / 'series.jpg')
-		fetchArtwork(series, 'fanart', language, seriesFolder / 'fanart.jpg')
-		fetchArtwork(series, 'season', language, seasonFolder / 'season.jpg')
-		fetchArtwork(series, 'seasonwide', language, seasonFolder / 'seasonwide.jpg')
+		if (season) {
+			fetchArtwork(series, 'posters', language, ['season', season], seasonFolder / 'poster.jpg')
+			fetchArtwork(series, 'banners', language, ['season', season], seasonFolder / 'banner.jpg')	
+		}
+		fetchArtwork(series, 'posters', language, ['series'], seriesFolder / 'poster.jpg')
+		fetchArtwork(series, 'banners', language, ['series'], seriesFolder / 'banner.jpg')
+		fetchArtwork(series, 'backgrounds', language, ['series'], seriesFolder / 'background.jpg')
+		fetchArtwork(series, 'clearart', language, ['series'], seriesFolder / 'clearart.png')
+		fetchArtwork(series, 'clearlogo', language, ['series'], seriesFolder / 'clearlogo.png')
+		fetchArtwork(series, 'icons', language, ['series'], seriesFolder / 'icon.png')
 		return
 	}
 
 	if (series.database == 'TheMovieDB::TV') {
-		fetchArtwork(series, 'posters', language, seriesFolder / 'poster.jpg')
-		fetchArtwork(series, 'backdrops', null, seriesFolder / 'backdrop.jpg')
+		fetchArtwork(series, 'posters', language, [], seriesFolder / 'poster.jpg')
+		fetchArtwork(series, 'backdrops', null, [], seriesFolder / 'backdrop.jpg')
 		return
 	}
 
@@ -54,24 +59,22 @@ def fetchEpisodeArtwork(episode, episodeFile) {
 }
 
 
-def fetchArtwork(object, category, language, file) {
+def fetchArtwork(object, category, language, tag, file) {
 	if (file.exists()) {
 		log.finest "[SKIP] Artwork already exists: $file"
 		return
 	}
 
-	def artwork = object.getArtwork(category, language)
-	if (artwork == null || artwork.empty) {
+	def artwork = object.getArtwork(category, language)?.find{ it.matches(*tag) }
+	if (artwork) {
+		log.fine "Fetch $artwork [$file]"
+		try {
+			artwork.url.saveAs(file)
+		} catch(e) {
+			log.severe "Failed to fetch artwork: $e.message"
+		}
+	} else {
 		log.warning "Artwork not found: $object [$file]"
-		return
-	}
-
-	def a = artwork[0]
-	log.fine "Fetch $a [$file]"
-	try {
-		a.url.saveAs(file)
-	} catch(e) {
-		log.severe "Failed to fetch artwork: $e.message"
 	}
 }
 
@@ -87,7 +90,7 @@ args.getFiles{ it.video }.each{ f ->
 			break
 		case Episode:
 			log.finest "[EPISODE] $m [$f]"
-			fetchSeriesArtwork(m.seriesInfo, m.seriesInfo.language, f.dir.dir, f.dir)
+			fetchSeriesArtwork(m.seriesInfo, m.seriesInfo.language, m.season, f.dir.dir, f.dir)
 			fetchEpisodeArtwork(m, f)
 			break;
 		default:
